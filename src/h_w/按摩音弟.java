9460850bd770ec6a7871f582345278006454af42
Creater <Creater@Creater-PC>
2016-10-05 22:56:52 +0800

    按摩音弟

---
+++ src/h_w/按摩音弟.java
@@ -0,0 +1,13 @@
+package h_w;
+
+public class «ö¼¯­µ§Ì {
+
+	public static void main(String[] args) {
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		String lyrics2 = lyrics.replace("it","\"her\"");
+		String lyrics3 = lyrics2.replace("Cannot","Can't");
+		System.out.println(lyrics3);
+
+	}
+
+}
